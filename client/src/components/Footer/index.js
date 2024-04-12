55ace18b8d5aff0d7fd55054207a897f510cac25
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-04-12 12:51:28 +0530

    home

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,9 +10,9 @@ import dribbble from "../../images/dribbble.svg"
 
 const Footer = () => {
   return (
-    <div className='p-[1em] footer-bg bg-slate-100 flex flex-col justify-between'>
+    <div className='p-[1em] footer-bg bg-slate-100'>
       <div className='p-[1em] footer-all-desc-bg'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col contact-footer'>
           <img className='h-[25px] w-[60px]' src={mainLogo} alt='' />
           <p>dribbble is the world's leading community for creatives to share,grow, and ger hired.</p>
           <div className='flex'>

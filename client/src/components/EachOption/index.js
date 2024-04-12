d078f2f447c3a1036457f6fc85714fe9647149b2
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-04-12 23:16:36 +0530

    profile func

--- client/src/components/EachOption/index.js
+++ client/src/components/EachOption/index.js
@@ -1,20 +1,37 @@
+import { useState } from 'react'
 import './index.css'
 
 
 const EachOption = (props) => {
   const {eachOption}  = props
   // console.log(eachOption)
-  const {image,desc,id} = eachOption
+  const {image,desc,id,para} = eachOption
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className='m-4 flex card
-    flex-col items-center'>
+    <div className={`m-4 flex card
+    flex-col items-center ${isChecked ? 'checked' : ''}`}>
       <img src={image}
-       className='mb-3 w-[280px]'
-       alt="designer" />     
-      <h1 className='desc-text w-[200px] mb-3 text-[17px] font-bold'>{desc}</h1>      
-      <div>
-        <input type="checkbox" id={id} className="select-option" />
-        <label htmlFor={id}></label>
+       className={`mb-3 w-[280px] ${isChecked?"card-image":""}`}
+       alt="designer"
+       />     
+
+      <div className='flex flex-col justify-center items-center
+       text-center
+       desc-card-profile'> 
+        <h1 className='desc-text w-[200px] mb-3 text-[17px] font-bold'>{desc}</h1>   
+        <p className='w-[250px]'>{isChecked?para:null}</p>   
+        <div>
+          <input type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          id={id} className="select-option" />
+          <label htmlFor={id}></label>
+        </div>
       </div>
     </div>
   )

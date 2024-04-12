cd7a56ca118fdaa195bdbcec96626319d88995ed
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-04-12 13:12:21 +0530

    not found component

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -5,14 +5,16 @@ import Profile from "../Profile/index"
 import "./index.css"
 import Options from "../Options/index"
 import Home from '../Home/index'
+import NotFound from "../NotFound/index"
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />  
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/options" element={<Options/>} />
-      <Route path="/" element={<Home/>} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/options" element={<Options />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }

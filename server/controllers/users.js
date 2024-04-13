8ee716d9e105ac9e0500cea35b1bfb55f53313c8
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-04-13 17:52:21 +0530

    sending mail 3 times

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 
-const Token = require("../models/token")
 const sendEmailId = require("../utils/sendEmail"); // Node.js doesn't support ES6 import statements natively yet. So, Use CommonJS syntax (require/module.exports)
 const crypto = require("crypto") // global module, so need not to install
 
@@ -103,22 +102,7 @@ exports.emailLogic = async (requestObject,responseObject)=>{
             responseObject.send("Invalid user");
         }
         if(existingUser){
-        // Check if there's already an existing token for the user
-        let token = await Token.findOne({ userId: existingUser._id });
-        if (token) {
-            // If a token already exists, update it
-            token.token = crypto.randomBytes(32).toString("hex");
-            await token.save();
-        } else {
-            // If no token exists, create a new one
-            token = await Token.create({
-                userId: existingUser._id,
-                token: crypto.randomBytes(32).toString("hex")
-            });
-        }
-            console.log(token,"token")
-    
-            const url = `${process.env.BASE_URL}${existingUser._id}/verify/${token.token}`
+            const url = `${process.env.BASE_URL}/${existingUser._id}/verify/`
             console.log(url,"url")
             await sendEmailId(emailId,"Verify Email",url)
             responseObject.status(201).send({message:"An Email sent to your account please verify"})
@@ -131,25 +115,3 @@ exports.emailLogic = async (requestObject,responseObject)=>{
 
 
 
-/*
-exports.verifyUser= async(responseObject,requestObject)=>{
-    console.log(requestObject.body)
-    try {
-        const user = await User.findOne(
-            {_id:requestObject.params.user_id}
-        )
-        if(!user) return responseObject.status(400).send({message:"Invalid link"})
-
-        const token = await Token.findOne({
-            userId:user._id,
-            token:requestObject.params.token
-        })
-        if(!token) return responseObject.status(400).send({message:"Invalid link"})
-        await User.updateOne({_id:user._id,verified:true})
-        await token.remove()
-        responseObject.status(200).send({message:"Email verified successfully"})
-    } catch (error) {
-        responseObject.status(500).send({message:"Internal server error"})
-    }
-}
-*/
--- server/routes/users.js
+++ server/routes/users.js
@@ -14,6 +14,5 @@ router.post("/login",usersController.loginUser)
 // email
 router.post("/sendemail",usersController.emailLogic)
 
-// router.get("/:id/verify/:token",usersController.verifyUser) // http://localhost:3000/6614fea60f4ffe2af73cb9f1/verify/12200ecdad5305d00c555rerr
 
 module.exports = router;
